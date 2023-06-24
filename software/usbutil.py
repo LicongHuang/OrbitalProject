dd45e701cda3b71903531ecf801874098b1e8115
LicongHuang <alexliconghuang@gmail.com>
2023-06-24 17:05:14 +0800

    C

--- software/usbutil.py
+++ software/usbutil.py
@@ -18,7 +18,7 @@ def fileWalk(path):
 # should find the usb from the usb ports
 def getUSBFilePath():
     # temp filefath
-    return "./usb_test"
+    return "/media/orangepi2/"
 
 def getUSBID():
     return ""
