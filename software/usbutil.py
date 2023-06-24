4e9aec3f19c62b44057eb0c60e19439a22666e27
LicongHuang <alexliconghuang@gmail.com>
2023-06-24 18:26:13 +0800

    Stuff

--- software/usbutil.py
+++ software/usbutil.py
@@ -1,18 +1,30 @@
 import os
 import encryption
 from os.path import join, getsize
+import subprocess
 # This is a function to get the file from the USB
+
 # state: 1 for getting the file from the USB
 
 
 def fileWalk(path):
     filesList = []
+
     for root, dirs, files in os.walk(path):
         print(root)
         print(files)
         for file in files:
             filesList.append(os.path.join(root, file))
     return filesList
+
+def fileWalk2(path):
+    fileList = []
+
+    command = f"ls -R {path}"
+    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    output = output.decode("utf-8").split("\n")
+    return output
+
 
 # Will implement the function later when on the hardware
 # should find the usb from the usb ports
