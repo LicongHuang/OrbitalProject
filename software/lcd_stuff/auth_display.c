07e762a509763d138fbcae5e00dd1d2cb4dba284
LicongHuang <alexliconghuang@gmail.com>
2023-07-16 04:46:07 +0800

    HELLLPPP

--- software/lcd_stuff/auth_display.c
+++ software/lcd_stuff/auth_display.c
@@ -13,32 +13,25 @@
 #define I2C_FILE_NAME "/dev/i2c-3"
 
 const char* txt[]  = {
-    "Password?"
+    "Password?",
+    ""
 };
 
-
-void display_letter(char* txt, int pos){
+int main(){
     int i2c_dev;
     lcd lcd0;
+
     // 0x27 is the address of the i2c device
     i2c_dev = open_i2c(I2C_FILE_NAME, 0x27);
     if(i2c_dev <0){
        printf("Errore: %d\n", i2c_dev);
-       return;
+       return 0 ;
     }
+
     lcd_init(&lcd0, i2c_dev);
     //lcd_clear(&lcd0);
-    lcd_print(&lcd0, txt, strlen(txt), pos);
+    lcd_print(&lcd0, txt[0], strlen(txt[0]), 0);
+    lcd_print(&lcd0, txt[1], strlen(txt[1]), 1);
     close_i2c(i2c_dev);
-    return ;
+    return 0;
 }
-
-int main(){
-  lcd lcd0;
-  lcd_clear(&lcd0);
-  display_letter((char*) txt[0], 0);
-  //display_letter((char*) txt[1], 1);
-  return 0;
-
-}
-
