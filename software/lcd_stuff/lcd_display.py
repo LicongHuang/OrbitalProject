4186215694c66f2e815839bbac7399dafb704a80
LicongHuang <alexliconghuang@gmail.com>
2023-07-16 03:56:40 +0800

    Help

--- software/lcd_stuff/lcd_display.py
+++ software/lcd_stuff/lcd_display.py
@@ -1,9 +1,9 @@
 from ctypes import *
 import os
 
-print(os.environ)
+print(os.path.abspath('./kp.so'))
 
-myFunct = CDLL('./kp.so')
+myFunct = CDLL(os.path.abspath('./kp.so'))
 myFunct.display_lcd.argtypes = [c_char_p]
 py_string = "Hello World"
 c_string = c_char_p(py_string.encode('utf-8'))
