4d02208661eeddf4c621d165da5d5782401849fc
LicongHuang <alexliconghuang@gmail.com>
2023-07-16 07:20:36 +0800

    EDITS

--- software/lcd_stuff/lcd_display.py
+++ software/lcd_stuff/lcd_display.py
@@ -1,7 +1,36 @@
 import os
 
-os.system("./auth_fail")
-os.system('./auth_display')
+def auth_fail():
+    os.system("./auth_fail")
+
+def auth_display():
+    os.system('./auth_display')
+
+def de_com():
+    os.system("./de_complete")
+
+def en_com():    
+    os.system("./en_complete")
+def en_or_de():
+    os.system("./en_or_de")
+
+def exit_c():
+    os.system("./exit_c")
+
+def invalid_in():
+    os.system("./invalid_in")
+
+def next_in():
+    os.system("./next_in")
+
+def empty_screen():
+    os.system("./empty_screen")
+
+def loading():
+    os.system("./loading")
+
+
+
 
 
 
